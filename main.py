eb6abb028565d6da7fb2bf709cdb2bf7134db5c6
Amir Parvinazar <110629402+imamirparvinazar@users.noreply.github.com>
2023-12-08 12:28:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import vpn
 
-# Instantiates the object
-vpn_server = vpn.VPNServer()
+kwargs = dict(
+    vpn_username="myusername",
+    vpn_password="Mypassword23$"
+)
+
+vpn_server = vpn.VPNServer(**kwargs)
 
 # Create a VPN Server
 vpn_server.create_vpn_server()
